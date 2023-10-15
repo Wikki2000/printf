f497cfceb27aa6d0a34509fb91175254434f5449
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-10-15 00:41:22 -0700

    Printf function updated.

--- _printf.c
+++ _printf.c
@@ -1,23 +1,6 @@
 #include "main.h"
 
 /**
- * print_string  - count the length of a string
- * @str: a string
- * Return: length of string
- */
-int print_string(char *str)
-{
-	int len = 0;
-
-	while (str[len] != '\0')
-	{
-		_putchar(str[len]);
-		len++;
-	}
-	return (len);
-}
-
-/**
  * _printf - print formatted output to standard output
  * @format: a string containing format specifiers and plain text
  *  Return: number of characters printed except null character
@@ -27,7 +10,6 @@ int _printf(const char *format, ...)
 {
 	va_list ls;
 	int i = 0, count = 0;
-	char *s, c;
 
 	va_start(ls, format);
 	if (format == NULL)
@@ -35,28 +17,24 @@ int _printf(const char *format, ...)
 	while (format[i] != '\0')
 	{
 		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-			count++;
-		}
+			count += print_character(format[i]);
 		else
 		{
 			i++;
-			switch (format[i])
+			if (format[i] == '\0')
+				return (-1);
+			if (format[i] == 'c')
+				count += print_character((char)va_arg(ls, int));
+			else if (format[i] == 's')
+				count += print_string(va_arg(ls, char *));
+			else if (format[i] == '%')
+				count += print_character(format[i]);
+			else if (format[i] == 'd' || format[i] == 'i')
+				count += print_decimal(va_arg(ls, int));
+			else
 			{
-			case 'c':
-				c = (char)va_arg(ls, int);
-				_putchar(c);
-				count++;
-				break;
-			case 's':
-				s = va_arg(ls, char *);
-				count += print_string(s);
-				break;
-			case '%':
-				_putchar(format[i]);
-				count++;
-				break;
+				count += print_character('%');
+				count += print_character(format[i]);
 			}
 		}
 		i++;
