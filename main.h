e21907de901b4847371fe9db889bfd8435051778
wisdomokposin@gmail.com <wikki2000>
2023-10-15 02:20:03 -0700

    Update maon.h file

--- main.h
+++ main.h
@@ -9,7 +9,6 @@ int _putchar(char c);
 int print_character(char c);
 int print_string(char *str);
 unsigned int print_binary(unsigned int decimal);
-unsigned int print_binary(unsigned int decimal);
 int print_decimal(int num);
 
 #endif
