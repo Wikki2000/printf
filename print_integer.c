aae9c63607d6a07f9b2bfa796a8537f235248751
wisdomokposin@gmail.com <wikki2000>
2023-10-15 09:58:45 -0700

    Update a file

--- print_integer.c
+++ print_integer.c
@@ -6,39 +6,19 @@
  */
 int print_decimal(int num)
 {
-	int len = 0, count = 0, temp = 0, l = 0, i = 0;
-	char *ptr;
+	int count = 0;
 
-	if (num == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
 	if (num < 0)
 	{
 		_putchar('-');
-		num = -(num);
 		count++;
+		num = (-num);
 	}
-	temp  = num;
-	while (temp > 0)
-	{
-		len++;
-		temp /= 10;
-	}
-	count += len;
-	ptr = malloc(len);
-	if (ptr == NULL)
-		return (-1);
-	l = len;
-	while (num > 0)
-	{
-		ptr[len] = '0' + (num % 10);
-		num /= 10;
-		len--;
-	}
-	for (i = 0; i <= l ; i++)
-		_putchar(ptr[i]);
-	free(ptr);
+
+	if (num / 10 != 0)
+		count += print_decimal(num / 10);
+	_putchar('0' + (num % 10));
+	count++;
+
 	return (count);
 }
