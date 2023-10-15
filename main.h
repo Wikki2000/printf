58405be0e013916d3ef68e72a6deca31d6477667
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-10-15 13:54:36 -0700

    Header file updated.

--- main.h
+++ main.h
@@ -9,7 +9,7 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int print_character(char c);
 int print_string(char *str);
-int print_binary(unsigned int num);
+int print_binary(unsigned int decimal);
 int print_decimal(int num);
 
 #endif
