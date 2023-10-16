9b9cb2b595fb239665eb10977f25cf400e4a625b
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-10-16 04:40:22 -0700

    All Files updated.

---
+++ _putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _putchar - Print a character one at a time
+ * @c: The character to be printed to the console
+ *
+ * This function print a character (c) one at a time
+ *
+ * Return: The character
+ */
+int _putchar(char c)
+{
+	return (putchar(c));
+}
--- main.h
+++ main.h
@@ -13,7 +13,5 @@ int print_binary(unsigned int decimal);
 int print_decimal(int num);
 int print_octal(unsigned int num);
 int print_unsigned(unsigned int num);
-int print_hexadecimal(int decimal);
-int print_HEXDACIMAL(int decimal);
 
 #endif
