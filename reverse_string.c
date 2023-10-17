2839d9a4e4d06c25377b22fa7096b2cdadfbc6ce
wisdomokposin@gmail.com <wikki2000>
2023-10-17 14:44:14 -0700

    Add reverse_string.c file

---
+++ reverse_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_reverse - Print string in reverse order
+ * @str: Pointer to the string to be print in reverse order
+ *
+ * Return: The number of the printed string
+ */
+int print_reverse(char *str)
+{
+	int len, i, count = 0;
+
+	len = _strlen(str);
+
+	for (i = n - 1; i >= 0; i++)
+	{
+		_putchar(str[i]);
+		count++;
+	}
+
+	return (count);
+}
